2d23a4c4ad4b695da1d7ca5735a70faabe72fdb3
Oleksandr Podvazhuk <43147435+inmistlosted@users.noreply.github.com>
2020-12-06 19:29:50 +0200

    added path

--- Lab3/setup.py
+++ Lab3/setup.py
@@ -2,13 +2,13 @@ from distutils.core import setup
 setup(
   name = 'Py2SQL',
   packages = ['Py2SQL'],
-  version = '0.3',
+  version = '0.4',
   license='mit',
   description = 'Package to work with firebirdSQL',
   author = 'Alexander Podvazhuk',
   author_email = 'sapod7@gmail.com',
   url = 'https://github.com/inmistlosted',
-  download_url = 'https://github.com/inmistlosted/metaprogramming/archive/0.3.tar.gz',
+  download_url = 'https://github.com/inmistlosted/metaprogramming/archive/0.4.tar.gz',
   keywords = ['Firebird', 'Py2SQL', 'SQL'],
   install_requires=[
           'fdb'

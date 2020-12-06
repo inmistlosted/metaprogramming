e1f323e4d77250b118723d108cb95eace577c65d
Oleksandr Podvazhuk <43147435+inmistlosted@users.noreply.github.com>
2020-12-06 19:05:52 +0200

    added files for pypi

--- Lab3/setup.py
+++ Lab3/setup.py
@@ -3,22 +3,21 @@ setup(
   name = 'Py2SQL',
   packages = ['Py2SQL'],
   version = '0.1',
-  license='afl-3.0',
+  license='mit',
   description = 'Package to work with firebirdSQL',
   author = 'Alexander Podvazhuk',
   author_email = 'sapod7@gmail.com',
   url = 'https://github.com/inmistlosted',
-  download_url = 'https://github.com/user/reponame/archive/v_01.tar.gz',    # I explain this later on
+  download_url = 'https://github.com/inmistlosted/metaprogramming/archive/0.1.tar.gz',
   keywords = ['Firebird', 'Py2SQL', 'SQL'],
-  install_requires=[            # I get to this in a second
-          'validators',
-          'beautifulsoup4',
+  install_requires=[
+          'fdb'
       ],
   classifiers=[
     'Development Status :: 4 - Beta',
     'Intended Audience :: Developers',
     'Topic :: Software Development :: Build Tools',
-    'License :: OSI Approved :: Academic Free License v3.0 License',
+    'License :: OSI Approved :: MIT License',
     'Programming Language :: Python :: 3',
     'Programming Language :: Python :: 3.4',
     'Programming Language :: Python :: 3.5',
